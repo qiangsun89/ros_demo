a6a82e58604f300a6fe3d4870e90b7a2ff76b61c
Qiang Sun <sunqiang891103@gmail.com>
2023-03-31 13:43:51 +0800

    add tf demo

---
+++ tf_demo/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/qiang/Documents/ros_demo_ws/src/ros_demo/tf_demo
+# Build directory: /home/qiang/Documents/ros_demo_ws/src/ros_demo/tf_demo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ tf_demo/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/qiang/Documents/ros_demo_ws/src/ros_demo/tf_demo/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/melodic/lib:/usr/local/lib'
+export PATH='/opt/ros/melodic/bin:/usr/local/bin:/home/qiang/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export ROS_PACKAGE_PATH="/home/qiang/Documents/ros_demo_ws/src/ros_demo/tf_demo:$ROS_PACKAGE_PATH"
